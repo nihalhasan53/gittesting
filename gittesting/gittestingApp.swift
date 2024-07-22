c4e1c5350f803927a3de8740b2db5b4b640c5ab5
Mohammed Nihal <nihalhasan53@gmail.com>
2024-07-22 17:55:35 +0530

    Initial Commit

---
+++ gittesting/gittestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestingApp.swift
+//  gittesting
+//
+//  Created by Nihal Hasan on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct gittestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
