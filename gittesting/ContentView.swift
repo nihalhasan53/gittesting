e097a5703e25e25b9486febaeefe41c8afa1ab4f
Mohammed Nihal <nihalhasan53@gmail.com>
2024-07-22 18:01:42 +0530

    feature 1

--- gittesting/ContentView.swift
+++ gittesting/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, main!")
+            Text("Hello, feature 1!")
         }
         .padding()
     }
